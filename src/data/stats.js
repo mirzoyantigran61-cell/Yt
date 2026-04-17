5de5585a4cd0ddb9853e9e8e4065b279a43a3a6c
mirzoyantigran61-cell <mirzoyantigran61@gmail.com>
2026-04-17 23:29:47 +0400

    Add stats.js with initial statistics data

---
+++ src/data/stats.js
@@ -0,0 +1,9 @@
+const stats = [
+  { value: '50K+', label: 'stats_users' },
+  { value: '99.99%', label: 'stats_uptime' },
+  { value: '10K+', label: 'stats_orders' },
+  { value: '50+', label: 'stats_countries' },
+  { value: '4.98', label: 'stats_rating' },
+]
+
+export default stats
